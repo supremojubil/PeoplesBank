8914f9b2c5da32dec37a364d9c5c70d0ffeb7b3e
Feric Decenan <feric.decenan.git@gmail.com>
2026-04-19 19:55:34 +0800

    partial connections tring

--- PeoplesBank/PeoplesBank.Data/ConnectionString.cs
+++ PeoplesBank/PeoplesBank.Data/ConnectionString.cs
@@ -1,5 +1,12 @@
-﻿namespace PeoplesBank.Data {
-    public class ConnectionString {
-        public static string ENTITY_CONNECTION_STRING = string.Empty;
+﻿
+namespace PeoplesBank.Data {
+    public partial class ConnectionString {
+        public static string ENTITY_CONNECTION_STRING =>
+            $"server=localhost;" +
+            $"port=3309;" +
+            $"database=peoplesbank;" +
+            $"uid=adminserver;" +
+            $"pwd=admin123!@#;" +
+            $"SSLMode=None;";
     }
 }
